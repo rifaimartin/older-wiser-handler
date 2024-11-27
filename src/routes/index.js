41acf7f9f3051d147ac9bcc02ed4508021628753
rifai.martin <rifai.martin@jagasuara.org>
2024-11-27 11:01:52 +0700

    verify catpcha

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,8 +3,11 @@ const express = require('express');
 const router = express.Router();
 const activityRoutes = require('./activity.routes');
 const auth = require('./auth.routes.js')
+const captcha = require('./captcha.routes.js')
 
 router.use('/activities', activityRoutes);
 router.use('/auth', auth);
+router.use('/captcha', captcha )
+
 
 module.exports = router;
